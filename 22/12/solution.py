9d4d4c422fe0f2de96f639d2c6f3d37849214df4
SpooperNooter <SpooperNooter@gmail.com>
2022-12-20 20:57:12 -0600

    changed 22-12

--- 22/12/solution.py
+++ 22/12/solution.py
@@ -1,28 +1,31 @@
-import numpy, math
-check = "0abcdefghijklmnopqrstuvwxyz"
-geomap = numpy.array([[check.index(x) if not x in "SE" else x for x in list(y)]for y in open("input.txt","r").read().splitlines()])
+import numpy, math, itertools, copy
+check = "0abcdefghijklmnopqrstuvwxyzSE"
+geomap = numpy.array([[check.index(x) for x in list(y)]for y in open("input.txt","r").read().splitlines()])
 height, width = geomap.shape
-ygrid,xgrid = numpy.mgrid[0:height,0:width]
+#ygrid,xgrid = numpy.mgrid[0:height,0:width]
+#
+#print(geomap.shape)
+#print(len(geomap))
+#
+#fullmap = numpy.dstack((xgrid,ygrid,geomap))
 
-print(geomap.shape)
-print(len(geomap))
 
-fullmap = numpy.dstack((xgrid,ygrid,geomap))
-
-
-neighbor = lambda y,x,value : (x,y, True if math.dist([value],[geomap[y,x]]) <= 1 else False) if ( x> -1) and (y > -1) else None
 def get_neighbors(set):
-  x,y, value = map(lambda x : x.astype(int).item(),set)
-  neighbors = [
-    neighbor(y-1,x,value),
-    neighbor(y,x+1,value),
-    neighbor(y+1,x,value),
-    neighbor(y,x-1,value)
-  ]
-  
-  return (x,y,value)
+  def neighbor(y,x,value):
+    try:
+      return (x,y, True if math.dist([value], [geomap[y, x].astype(int).item()]) <= 1 else False) if ( x> -1) and (y > -1) else False
+    except IndexError:
+      pass
+  value = geomap[y,x].astype(int).item()
+  return (
+    (0,-1) if neighbor(y-1,x,value) else (0,0),
+    (1,0) if neighbor(y,x+1,value) else(0,0),
+    (0, 1) if neighbor(y+1,x,value) else (0,0),
+    (-1, 0) if neighbor(y,x-1,value) else (0,0)
+  )
+#
+#
+#print(numpy.extract(numpy.apply_along_axis(lambda x: True if "S" in x else False, 2, fullmap), fullmap))
 
-neighbor_map = numpy.apply_along_axis(get_neighbors,2,fullmap)
-
-print(neighbor_map)
-
+y,x = numpy.where(geomap==27)
+pos = {x.tolist() + y.tolist(),}
