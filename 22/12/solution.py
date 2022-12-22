8acb3cb9e8025e8e18375a8bfa9e83be68a7d586
Quinn Nguyen <70044204+SpooperNooter@users.noreply.github.com>
2022-12-22 17:56:04 +0000

    changed 22-12

--- 22/12/solution.py
+++ 22/12/solution.py
@@ -1,19 +1,16 @@
-import numpy, math, itertools, copy
-check = "0abcdefghijklmnopqrstuvwxyzSE"
+import numpy, math, itertools, copy, operator, multiprocessing
+check = "SabcdefghijklmnopqrstuvwxyzE"
 geomap = numpy.array([[check.index(x) for x in list(y)]for y in open("input.txt","r").read().splitlines()])
 height, width = geomap.shape
-#ygrid,xgrid = numpy.mgrid[0:height,0:width]
-#
-#print(geomap.shape)
-#print(len(geomap))
-#
-#fullmap = numpy.dstack((xgrid,ygrid,geomap))
-
+counter_map = numpy.array([[-1 for x in range(width)] for y in range(height)])
 
 def get_neighbors(set):
+  x,y = set
   def neighbor(y,x,value):
     try:
-      return (x,y, True if math.dist([value], [geomap[y, x].astype(int).item()]) <= 1 else False) if ( x> -1) and (y > -1) else False
+      #print(value, geomap[y,x])
+      #print(math.dist([value],[geomap[y,x].astype(int).item()])<=1)
+      return True if math.dist([value], [geomap[y, x].astype(int).item()]) <= 1 else False if ( x> -1) and (y > -1) else None
     except IndexError:
       pass
   value = geomap[y,x].astype(int).item()
@@ -27,5 +24,46 @@ def get_neighbors(set):
 #
 #print(numpy.extract(numpy.apply_along_axis(lambda x: True if "S" in x else False, 2, fullmap), fullmap))
 
-y,x = numpy.where(geomap==27)
-pos = {x.tolist() + y.tolist(),}
+#def check(set, _queue):
+ # for x in map(lambda x,y: tuple(map(operator.add, x,y)), itertools.cycle([set]),get_neighbors(set)):
+  #  _queue.put(x)
+
+if __name__ == "__main__":
+  y,x = numpy.where(geomap==0)
+  endy, endx = numpy.where(geomap==27)
+  pos = {tuple(x.tolist() + y.tolist())}
+  end = tuple(endx.tolist() + endy.tolist())
+  visited = {tuple(x.tolist()+y.tolist())}
+  _queue = []
+
+  #print(get_neighbors((0,2)))
+  
+  cycle = 0
+  with multiprocessing.Pool() as _pool:
+    try:
+      while True:
+        print(cycle)
+        _pos = copy.copy(pos)
+        pos.clear()
+        #_pool.starmap(check,zip(pos,itertools.cycle([_queue])))
+        for _set in _pos:
+          _queue.extend(map(lambda x,y: tuple(map(operator.add, x,y)), itertools.cycle([_set]),get_neighbors(_set)))
+        new = set(_queue).difference(visited)
+        pos = new
+        
+        for x in new:
+          visited.add(x)
+          if counter_map[x[1],x[0]] == -1:
+            counter_map[x[1], x[0]] = cycle
+        if end in visited:
+          print(cycle+1)
+          print(counter_map)
+          raise KeyboardInterrupt
+        cycle += 1
+    except KeyboardInterrupt:
+      pass
+  
+        
+        
+        
+
