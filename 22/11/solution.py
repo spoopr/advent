124d64e28ce732807bd77f1d391db9536374281d
Quinn Nguyen <70044204+SpooperNooter@users.noreply.github.com>
2023-01-12 18:43:04 +0000

    solved 22-11

--- 22/11/solution.py
+++ 22/11/solution.py
@@ -1,70 +1,53 @@
-import itertools, math, operator, copy
-from pathos.multiprocessing import ProcessingPool as Pool
+import numpy as np
+import math
 
+input = [a.splitlines() for a in open('input.txt',"r").read().split("\n\n")]
 
-class monkey:
-    def __init__(self, text):
-        self.items = [int(x) for x in text[1].replace("Starting items:", "").replace(" ", "").split(",")]
-        self.operation = eval(f"lambda old : math.floor(({text[2].split('=')[1]})%10000)")
-        self.test_num = int(text[3].split(" ")[-1])
-        self.test_true = f"Monkey {text[4].split(' ')[-1]}"
-        self.test_false = f"Monkey {text[5].split(' ')[-1]}"
-        self.inspected = 0
+items = [eval("["+monkey[1].replace("  Starting items: ","")+"]") for monkey in input]
 
+tests = [(int(monkey[3].replace("  Test: divisible by ","")), int(monkey[4].replace("    If true: throw to monkey ","")), int(monkey[5].replace("    If false: throw to monkey ",""))) for monkey in input]
 
-def test(_monkey, num):
-    if (changed_num := _monkey.operation(num)) % _monkey.test_num == 0:
-        # monkeys[_monkey.test_true].items.append((changed_num,))
-        return (_monkey.test_true, changed_num)
-    else:
-        # monkeys[_monkey.test_false].items.append((changed_num,))
-        return (_monkey.test_false, changed_num)
+operations = [monkey[2].replace('  Operation: new = ','').replace("old","item") for monkey in input]
 
+_a = f"""
+def operate(monkey, item):
+  if monkey == 0:
+    return {input[0][2].replace('  Operation: new = ','').replace("old","item")}
+  elif monkey == 1:
+    return {input[1][2].replace('  Operation: new = ','').replace("old","item")}
+  elif monkey == 2:
+    return {input[2][2].replace('  Operation: new = ','').replace("old","item")}
+  elif monkey == 3:
+    return {input[3][2].replace('  Operation: new = ','').replace("old","item")}
+  elif monkey == 4:
+    return {input[4][2].replace('  Operation: new = ','').replace("old","item")}
+  elif monkey == 5:
+    return {input[5][2].replace('  Operation: new = ','').replace("old","item")}
+  elif monkey == 6:
+    return {input[6][2].replace('  Operation: new = ','').replace("old","item")}
+  elif monkey == 7:
+    return {input[7][2].replace('  Operation: new = ','').replace("old","item")}
+"""
 
-monkeys = {(text := monkeytext.splitlines())[0][:-1]: monkey(text) for monkeytext in open("input.txt", "r").read().split("\n\n")}
+exec(_a)
 
+inspected = [0 for monkey in input]
+big_mod = np.prod([int(monkey[3].split(" ")[-1]) for monkey in input])
 
-def run(_monkey, num):
-    _monkeys = {x: 0 for x in monkeys}
-    for x in range(10000):
-        while True:
-            b = _monkey
-            _monkeys[_monkey] += 1
-            _monkey, num = test(monkeys[_monkey], num)
-            if int(b[-1]) >= int(_monkey[-1]):
-                break
-    return (_monkeys.values(), num)
+for x in range(10000):
+  print(x)
+  for monkey, inventory in enumerate(items):
+    for item in inventory:
+      item = math.floor(operate(monkey, item)) % big_mod
+      inspected[monkey] += 1
+      if item % tests[monkey][0] == 0:
+        items[tests[monkey][1]].append(item)
+      else:
+        items[tests[monkey][2]].append(item)
+    items[monkey] = []
 
+print(inspected)
+print(items)
+inspected.sort(reverse=True)
 
-if __name__ == "__main__":
-    totals = [0 for x in monkeys]
-    with Pool(nodes=8) as _pool:
-        a = [monkey for monkey, _class in monkeys.items() for item in _class.items]
-        b = [item for monkey, _class in monkeys.items() for item in _class.items]
-        originals = copy.copy(b)
-        print(a, b)
-        items = _pool.map(run, a, b)
-        b = []
-        for total, num in items:
-            b.append(num)
-            print(total, totals)
-            totals = map(operator.add, totals, total)
-
-    totals = sorted(list(totals), reverse=True)
-    print(b)
-
-    print(totals[0] * totals[1])
-    print(run("Monkey 0", 79))
-
-# [3,0,0,2]
-# for roundNum in range(10000):
-#  d += 1
-#  print(d)
-#  for _monkey in monkeys.values():
-#    b = list(map(test, itertools.cycle([_monkey]), _monkey.items))
-#    _monkey.inspected += len(_monkey.items)
-#    _monkey.items = []
-
-# a = sorted([_monkey.inspected for _monkey in monkeys.values()], reverse=True)
-# print(monkeys["Monkey 0"].items)
-# print(f"\n{a[0] * a[1]}")
+print(inspected[0]*inspected[1])
