95b66cb4d546fd7d67300ad0ba23ec3dbc4b3b49
spoopr <spoopr@users.noreply.github.com>
2024-12-25 21:49:05 -0600

    solved day 7

---
+++ 7/solution2.py
@@ -0,0 +1,22 @@
+import re
+
+input = open("input.txt", "r+").read().splitlines()
+
+def branchCheck(target, value, numbers):
+	if len(numbers) == 0:
+		return value == target
+	
+	elif value > target:
+		return False
+	
+	return branchCheck(target, value + numbers[0], numbers[1:]) or branchCheck(target, value * numbers[0], numbers[1:]) or branchCheck(target, value * (10**len(str(numbers[0]))) + numbers[0], numbers[1:])
+		
+
+total = 0
+for equation in input:
+	target, *numbers = [int(x) for x in re.findall(r"\d+", equation)]
+	if branchCheck(target, numbers[0], numbers[1:]):
+		total += target
+
+print(total)	
+	
